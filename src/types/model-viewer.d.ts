7986719e1a4600a93a9c62b0bc6ac44d47695e12
Esraa Soliman <ee341110@gmail.com>
2026-04-12 15:30:37 +0200

    fixes

---
+++ src/types/model-viewer.d.ts
@@ -0,0 +1,21 @@
+import React from "react";
+
+declare module "react" {
+  namespace JSX {
+    interface IntrinsicElements {
+      "model-viewer": React.DetailedHTMLProps<
+        React.HTMLAttributes<HTMLElement>,
+        HTMLElement
+      > & {
+        src?: string;
+        alt?: string;
+        "auto-rotate"?: boolean;
+        "camera-controls"?: boolean;
+        autoplay?: boolean;
+        ar?: boolean;
+        "shadow-intensity"?: string | number;
+        "environment-image"?: string;
+      };
+    }
+  }
+}
